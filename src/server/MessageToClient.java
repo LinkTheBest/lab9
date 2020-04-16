191e29178051d19af46e8845804a4c3deaaacaa0
Linktech <popov.n294@gmail.com>
2020-04-16 17:49:56 +0300

    New constructor and getter

--- src/server/MessageToClient.java
+++ src/server/MessageToClient.java
@@ -1,13 +1,17 @@
 package server;
 
+import commandsRealization.ListOfCommands;
 import spaceMarineProperties.SpaceMarine;
 
 import java.io.Serializable;
 import java.util.ArrayDeque;
+import java.util.List;
 
 public class MessageToClient implements Serializable {
+    private SpaceMarine spaceMarine;
     private String message;
     private ArrayDeque<SpaceMarine> spaceMarines;
+    private List<String> helpArray;
 
     public MessageToClient(String message){
         this.message = message;
@@ -18,9 +22,15 @@ public class MessageToClient implements Serializable {
         this.spaceMarines = spaceMarines;
     }
 
+    public MessageToClient(String message, List<String> helpArray){
+        this.message = message;
+        this.helpArray = helpArray;
+    }
+
     public ArrayDeque<SpaceMarine> getSpaceMarines(){
         return spaceMarines;
     }
+    public List<String> getHelpArray(){return helpArray;}
     public String getMessage(){
         return message;
     }
