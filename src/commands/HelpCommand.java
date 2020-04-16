0c4e7554d64bd9eccaec22d42fbbcedab68dc1ac
Linktech <popov.n294@gmail.com>
2020-04-16 17:46:12 +0300

    Changed Map to List

--- src/commands/HelpCommand.java
+++ src/commands/HelpCommand.java
@@ -5,9 +5,7 @@ import server.Collection;
 import server.MessageToClient;
 import server.TbI_PROSTO_SUPER;
 
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
+import java.util.*;
 
 public class HelpCommand extends FatherOfCommands {
     public HelpCommand(Collection collection, TbI_PROSTO_SUPER kryto) {
@@ -16,29 +14,24 @@ public class HelpCommand extends FatherOfCommands {
 
     @Override
     public MessageToClient executeCommand(Command command) {
-        Map<String, String> commandList = new HashMap<>();
-        commandList.put("help", "вывести справку по доступным командам");
-        commandList.put("info", "вывести в стандартный поток вывода информацию о коллекции");
-        commandList.put("show", "вывести в стандартный поток вывода все элементы коллекции в строковом представлении");
-        commandList.put("add {element}", "добавить новый элемент в коллекцию");
-        commandList.put("update_id {element}", "обновить значение элемента коллекции, id которого равен заданному");
-        commandList.put("remove_by_id id", "удалить элемент из коллекции по его id");
-        commandList.put("clear", "очистить коллекцию");
+        List<String> helpArray = new ArrayList<>();
+        helpArray.add("help: вывести справку по доступным командам");
+        helpArray.add("info: вывести в стандартный поток вывода информацию о коллекции");
+        helpArray.add("show: вывести в стандартный поток вывода все элементы коллекции в строковом представлении");
+        helpArray.add("add {element}: добавить новый элемент в коллекцию");
+        helpArray.add("update_id {element}: обновить значение элемента коллекции, id которого равен заданному");
+        helpArray.add("remove_by_id id: удалить элемент из коллекции по его id");
+        helpArray.add("clear: очистить коллекцию");
         //commandList.put("save", "сохранить коллекцию в файл");
-        commandList.put("execute_script file_name", "считать и исполнить скрипт из указанного файла. В скрипте содержатся команды в таком же виде, в котором их вводит пользователь в интерактивном режиме.");
+        helpArray.add("execute_script file_name: считать и исполнить скрипт из указанного файла. В скрипте содержатся команды в таком же виде, в котором их вводит пользователь в интерактивном режиме.");
         //commandList.put("exit", "завершить программу (без сохранения в файл)");
-        commandList.put("add_if_max {element}", "добавить новый элемент в коллекцию, если его значение превышает значение наибольшего элемента этой коллекции");
-        commandList.put("add_if_min {element}", "добавить новый элемент в коллекцию, если его значение меньше, чем у наименьшего элемента этой коллекции");
-        commandList.put("remove_lower {element}", "удалить из коллекции все элементы, меньшие, чем заданный");
-        commandList.put("sum_of_health", "вывести сумму значений поля health для всех элементов коллекции");
-        commandList.put("print_descending", "вывести элементы коллекции в порядке убывания");
-        commandList.put("print_descending_health {health}", "вывести значения поля health в порядке убывания");
+        helpArray.add("add_if_max {element}: добавить новый элемент в коллекцию, если его значение превышает значение наибольшего элемента этой коллекции");
+        helpArray.add("add_if_min {element}: добавить новый элемент в коллекцию, если его значение меньше, чем у наименьшего элемента этой коллекции");
+        helpArray.add("remove_lower {element}: удалить из коллекции все элементы, меньшие, чем заданный");
+        helpArray.add("sum_of_health: вывести сумму значений поля health для всех элементов коллекции");
+        helpArray.add("print_descending: вывести элементы коллекции в порядке убывания");
+        helpArray.add("print_descending_health {health}: вывести значения поля health в порядке убывания");
 
-        Iterator<Map.Entry<String, String>> iterator = commandList.entrySet().iterator();
-        while (iterator.hasNext()) {
-            Map.Entry<String, String> entry = iterator.next();
-            System.out.println(entry.getKey() + " : " + entry.getValue());
-        }
-        return new MessageToClient("");
+        return new MessageToClient("",helpArray);
     }
 }
