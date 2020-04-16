f2e95cec4183651b47750e34f9484a5c7fd38573
Linktech <popov.n294@gmail.com>
2020-04-16 17:41:53 +0300

    Added colors to ouput

--- src/client/ClientConnection.java
+++ src/client/ClientConnection.java
@@ -8,7 +8,8 @@ public class ClientConnection {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         System.out.println("Для начала работы введите порт или 'exit.'");
-        System.out.println("PORT: ");
+        System.out.print(Colors.GREEN_BOLD);
+        System.out.print("PORT: ");
         while (true) {
             if (!scn.hasNextLine()) {
                 break;
@@ -21,12 +22,17 @@ public class ClientConnection {
                 try {
                     int port = Integer.valueOf(userInput);
                     if (port < 0 || port >= 65535) {
+                        System.out.print(Colors.RED_BOLD);
                         System.out.println("Invalid port!");
                     } else {
                         try {
                             ClientMain clientMain = new ClientMain(port);
                             clientMain.start();
                         } catch (IOException e) {
+                            System.out.print(Colors.RED_BOLD);
+                            System.out.println("Ошибка");
+                            System.out.printf(e.getMessage());
+                            System.out.print(Colors.CYAN_BOLD);
                         }
 
                     }
