dd5c71028e85bf0d10d218c817088a8c6117491f
Linktech <popov.n294@gmail.com>
2020-04-16 17:50:11 +0300

    io update

--- src/server/ServerConnection.java
+++ src/server/ServerConnection.java
@@ -1,5 +1,7 @@
 package server;
 
+import client.Colors;
+
 import java.io.IOException;
 import java.util.Scanner;
 
@@ -8,8 +10,10 @@ public class ServerConnection {
 
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
+        System.out.print(Colors.CYAN_BOLD);
         System.out.println("Для начала работы введите порт или 'exit.'");
-        System.out.println("PORT: ");
+        System.out.print(Colors.GREEN_BOLD);
+        System.out.print("PORT: ");
         while(true) {
             if (!scn.hasNextLine()) {
                 break;
@@ -22,12 +26,18 @@ public class ServerConnection {
                 try{
                     int port = Integer.valueOf(userInput);
                     if(port < 0 || port >= 65535){
+                        System.out.print(Colors.RED_BOLD);
                         System.out.println("Invalid port!");
                     } else{
                         try {
                             ServerMain serverMain = new ServerMain(port, FILE_NAME);
                             serverMain.start();
                         }catch (IOException e){
+                            System.out.print(Colors.RED_BOLD);
+                            System.out.println("Ошибка");
+                            System.out.printf(e.getMessage());
+                            System.out.print(Colors.CYAN_BOLD);
+
                         }catch (ClassNotFoundException e){}
 
                     }
