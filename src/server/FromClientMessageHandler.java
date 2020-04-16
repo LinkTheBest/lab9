d513a8325081e3bb2c708cc2802026c580ff9027
Linktech <popov.n294@gmail.com>
2020-04-16 17:48:54 +0300

    io update

--- src/server/FromClientMessageHandler.java
+++ src/server/FromClientMessageHandler.java
@@ -7,23 +7,23 @@ import commandsRealization.Command;
 import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.net.ServerSocket;
 import java.net.Socket;
 
 public class FromClientMessageHandler {
-    private Socket socket;
+    private Socket clientSocket;
 
     public FromClientMessageHandler(Socket socket) {
-        this.socket = socket;
+        clientSocket = socket;
     }
 
     public Command getMessage() throws IOException, ClassNotFoundException {
-        BufferedInputStream inputStream = new BufferedInputStream(socket.getInputStream());
+        BufferedInputStream inputStream = new BufferedInputStream(clientSocket.getInputStream());
         ObjectInputStream objectInputStream = new ObjectInputStream(inputStream);
         Command command = (Command) objectInputStream.readObject();
-        System.out.print(Colors.CYAN_BOLD);
+        System.out.print(Colors.GREEN_BOLD);
         System.out.println("Была получена команда:" + command.getCommand());
-        objectInputStream.close();
-        socket.close();
+        //objectInputStream.close();
         return command;
     }
 }
