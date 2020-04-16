a99518418f6c44f43ada9ad7eaea8e3fd3f7ca4b
Linktech <popov.n294@gmail.com>
2020-04-16 17:42:30 +0300

    Added colors to ouput Updated io code

--- src/client/ClientMain.java
+++ src/client/ClientMain.java
@@ -23,13 +23,14 @@ public class ClientMain {
 
     public void start() {
         System.out.print(Colors.CYAN);
-        System.out.println("Для ознакомлением со списком команд, введите 'info'");
+        System.out.println("Для ознакомлением со списком команд, введите 'help'");
         while (!userCommand.equals("exit")) {
-            System.out.println("Введите команду: ");
-            userCommand = userInput.nextLine();
+            System.out.print(Colors.GREEN_BOLD);
+            System.out.print("Введите команду: ");
             if (!userInput.hasNextLine()) {
                 break;
             } else {
+                userCommand = userInput.nextLine();
                 try {
                     fromStringToCommand = new FromStringToCommand();
                     Command command = fromStringToCommand.getCommandFromString(userCommand);
@@ -37,30 +38,42 @@ public class ClientMain {
                     fromServerMessageHandler = new FromServerMessageHandler(clientSocket);
                     toServerMessageHandler.sendMessage(command);
                     MessageToClient message = fromServerMessageHandler.getMessage();
-                    System.out.println("asdfasfhsadhksadf"+message.getMessage());
                     if (message != null) {
                         if (message.getMessage() != null) {
-                            System.out.print(Colors.CYAN_BOLD);
-                            System.out.print("Сервер: ");
-                            System.out.println(message.getMessage());
+                            if (message.getMessage() == " ") {
+                            } else {
+                                System.out.print(Colors.GREEN_BOLD);
+                                System.out.print("Сообщение от сервера: ");
+                                System.out.print(Colors.BLACK_BOLD);
+                                System.out.println(message.getMessage());
+                            }
                         } else {
+                            System.out.print(Colors.GREEN_BOLD);
                             System.out.println("От сервера нет ответа");
                         }
                         if (message.getSpaceMarines() != null) {
                             for (SpaceMarine scp : message.getSpaceMarines()) {
-                                System.out.println(scp);
+                                System.out.print(Colors.BLACK_BOLD);
+                                System.out.println(scp.toString());
                             }
+                            System.out.print(Colors.CYAN_BOLD);
+                        }
+                        if (message.getHelpArray() != null) {
+                            for (String str : message.getHelpArray()) {
+                                System.out.print(Colors.BLACK_BOLD);
+                                System.out.println(str);
+                            }
+                            System.out.print(Colors.CYAN_BOLD);
                         }
                     } else {
+                        System.out.print(Colors.GREEN_BOLD);
                         System.out.println("Ответ некорректен");
                     }
                 } catch (IOException e) {
                 } catch (ClassNotFoundException e) {
                 }
             }
-
         }
-
     }
 }
 
