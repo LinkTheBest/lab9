7fe98ee5d9a98b4b203c468679b4bd69c6e30152
Linktech <popov.n294@gmail.com>
2020-04-18 13:32:08 +0300

    update

--- src/server/ToClientMessageHandler.java
+++ src/server/ToClientMessageHandler.java
@@ -1,8 +1,5 @@
 package server;
 
-import client.Colors;
-import spaceMarineProperties.SpaceMarine;
-
 import java.io.*;
 import java.net.Socket;
 
@@ -19,15 +16,11 @@ public class ToClientMessageHandler {
         objectOutputStream.writeObject(message);
         objectOutputStream.flush();
         if(message.getMessage().equals("")) {
-            return "Отправлено клиенту\n";
+            return "Отправлено клиенту";
         } else {
             return "Клиенту отправлено сообщение:" + message.getMessage() + "\n";
         }
     }
 
-    public void send() throws IOException {
-        OutputStreamWriter outputStream = new OutputStreamWriter(clientSocket.getOutputStream());
-        outputStream.write("Отправка сообщения отменена");
-    }
 
 }
