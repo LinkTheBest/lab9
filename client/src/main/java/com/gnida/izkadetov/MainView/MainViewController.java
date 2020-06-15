b6711438adb8519880df0d0e5405d8663ca2754d
Linktech <popov.n294@gmail.com>
2020-06-15 20:37:35 +0300

    mainview and main controller update

--- client/src/main/java/com/gnida/izkadetov/MainView/MainViewController.java
+++ client/src/main/java/com/gnida/izkadetov/MainView/MainViewController.java
@@ -1,4 +1,56 @@
 package com.gnida.izkadetov.MainView;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+
 public class MainViewController {
+    @FXML
+    private Pane userInfoPane;
+
+    @FXML
+    private Pane commandsButtonPane;
+
+    @FXML
+    private Button logoutButton;
+
+    @FXML
+    private Button addButton;
+
+    @FXML
+    private Button addMaxButton;
+
+    @FXML
+    private Button addMinButton;
+
+    @FXML
+    private Button removeLowerButton;
+
+    @FXML
+    private Button removeByIdButton;
+
+    @FXML
+    private Button clearButton;
+
+    @FXML
+    private Button infoButton;
+
+    @FXML
+    private Button helpButton;
+
+    @FXML
+    private Button executeButton;
+
+    @FXML
+    private Button sumButtton;
+
+    @FXML
+    private Button printDesButton;
+
+    @FXML
+    private Button printDeshealthButton;
+
+    @FXML
+    private Button exitButton;
+
 }
